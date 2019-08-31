60af6103a379ac662d64f2d1221fe85dfe0b2bb1
etoos <49575896+etoos@users.noreply.github.com>
2019-08-31 11:00:41 +0900

    Add files via upload

---
+++ float.py
@@ -0,0 +1,43 @@
+
+
+bangdream! girls band party!                1
+
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/float(3)
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/3
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))               2
+
+2.¹®ÀÚ¿­
+
+
+
+print("{:s}".format('I\'m enjoying learning Python.'))
+
+print("{0:s}".format("This is a long string.Without the backslash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit. By using the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole sring is easy\
+to view in the editor."))
+
+print("{0:s}".format('''you can use triple single quotes
+for multi-line comment strings,'''))
+
+print("{:s}".format("""You can also use triple double quotes 
+for multi-line comment strings."""))                                                 3
+ 
+integer.py   float.py string.py          
+
+
+
