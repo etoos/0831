3dbb61fd8256597e4f2cf4f730aa12cd8e3bdcac
etoos <49575896+etoos@users.noreply.github.com>
2019-08-31 12:29:28 +0900

    Add files via upload

---
+++ list01.py
@@ -0,0 +1,107 @@
+
+
+bangdream! girls band party!                1
+
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/float(3)
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/3
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))               2
+
+2.¹®ÀÚ¿­
+
+
+
+print("{:s}".format('I\'m enjoying learning Python.'))
+
+print("{0:s}".format("This is a long string.Without the backslash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit. By using the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole sring is easy\
+to view in the editor."))
+
+print("{0:s}".format('''you can use triple single quotes
+for multi-line comment strings,'''))
+
+print("{:s}".format("""You can also use triple double quotes 
+for multi-line comment strings."""))                                                 3
+ 
+integer.py   float.py string.py  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string1 = "This is a"
+string2 = "short string."
+sentence = string1 + string2
+print("{}".format(sentence))
+print("{}{}{}".format("She is","very"*4,"beautiful."))
+m = len(sentence)
+print("{}".format(m))
+string1 = "My deliveralbe is due in May"
+string1_list1 = string1.split()
+string1_list2 = string1.split(" ",2)
+print(string1_list1)
+print("FIRST PIECE:{0}SECOND PIECE::{1}THIRD PIECE:{2}"\
+.format(string1_list2[0], string1_list2[1], string1_list2[2]))
+string2 = "Your,deliveralbe,is,due,in,June"
+string2_list=string2.split(',')
+print(string2_list)
+print("{}{}{}{}".format(string2_list[1],string2_list[2],string2_list[5],\
+string2_list[-1]))
+print("{}".format(','.join(string2_list)))
+
+
+list01.py
+
+a_list=[1,2,3]
+print(a_list)
+print("a_list has {} elements.".format(len(a_list)))
+print("the maximum value in a_list is {}.".format(max(a_list)))
+print("the minimum value in a_list is {}.".format(min(a_list)))
+another_list=['printer',5,['star','circle',9]]
+print(another_list)
+print("another_list also has {} elements.".format(len(another_list)))
+print("5 is in another_list {} time.".format(another_list.count(5)))
+print(a_list[0])
+print(a_list[1])
+print(a_list[2])
+print(a_list[-1])
+print(a_list[-2])
+print(a_list[0:2])
+
+tuple.py
+
+
+
+
+
+        
+
+
+
---
+++ strop.py
@@ -0,0 +1,90 @@
+
+
+bangdream! girls band party!                1
+
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/float(3)
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))
+
+
+print("{0:.3f}".format(9.3/2.7))
+y=2.5 * 4.8
+print("{:.1f}".format(y))
+r = 8/3
+print("{:.2f}".format(r))
+print("{0:.4f}".format(8.0/3))               2
+
+2.¹®ÀÚ¿­
+
+
+
+print("{:s}".format('I\'m enjoying learning Python.'))
+
+print("{0:s}".format("This is a long string.Without the backslash\
+it would run off of the page on the right in the text editor and be very\
+difficult in read and edit. By using the backslash you can split the long\
+string into smaller strings on seperate lines so that the whole sring is easy\
+to view in the editor."))
+
+print("{0:s}".format('''you can use triple single quotes
+for multi-line comment strings,'''))
+
+print("{:s}".format("""You can also use triple double quotes 
+for multi-line comment strings."""))                                                 3
+ 
+integer.py   float.py string.py  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+string1 = "This is a"
+string2 = "short string."
+sentence = string1 + string2
+print("{}".format(sentence))
+print("{}{}{}".format("She is","very"*4,"beautiful."))
+m = len(sentence)
+print("{}".format(m))
+string1 = "My deliveralbe is due in May"
+string1_list1 = string1.split()
+string1_list2 = string1.split(" ",2)
+print(string1_list1)
+print("FIRST PIECE:{0}SECOND PIECE::{1}THIRD PIECE:{2}"\
+.format(string1_list2[0], string1_list2[1], string1_list2[2]))
+string2 = "Your,deliveralbe,is,due,in,June"
+string2_list=string2.split(',')
+print(string2_list)
+print("{}{}{}{}".format(string2_list[1],string2_list[2],string2_list[5],\
+string2_list[-1]))
+print("{}".format(','.join(string2_list)))
+
+
+list01.py
+
+
+
+
+
+
+        
+
+
+
